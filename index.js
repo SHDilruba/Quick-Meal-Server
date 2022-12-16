8a96a84911142435d4d4aa9e89206d5a266ab879
unknown <hasinadilruba2021@gmail.com>
2022-12-16 21:14:36 +0100

    get service data by id

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ async function run(){
         res.send(services);
       });
 
+      app.get('/services/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const service = await serviceCollection.findOne(query);
+        res.send(service);
+      });
+
   }
   finally{
   }
