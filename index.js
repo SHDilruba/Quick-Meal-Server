016f7b38fc1dc9ada56efa726b46f3a44155432a
unknown <hasinadilruba2021@gmail.com>
2022-12-16 21:30:48 +0100

    jwt token created

--- index.js
+++ index.js
@@ -14,11 +14,33 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+    if(err){
+      return res.status(403).send({message: 'forbidden access'})
+    }
+      req.decoded = decoded;
+      next();
+  })
+}
+
 async function run(){
   try{
       const serviceCollection = client.db('quickMeal').collection('services');
       const benefitCollection = client.db('quickMeal').collection('benefits');
       const blogCollection = client.db('quickMeal').collection('blog');
+      const reviewCollection = client.db('quickMeal').collection('reviews');
+
+      app.post('/jwt', (req, res) =>{
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '7d'})
+        res.send({token})
+     })
 
         app.get('/services', async(req, res) =>{
           const query = {}
@@ -48,6 +70,13 @@ async function run(){
         service._id = service.insertedId;
         res.send(service);
      });
+
+     app.get('/benefits', async(req, res) =>{
+      const query = {}
+      const cursor = benefitCollection.find(query);
+      const benefits = await cursor.toArray();
+      res.send(benefits);
+    });
 
      app.get('/blog', async(req, res) =>{
       const query = {}
