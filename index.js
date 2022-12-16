2a085aaf6a994f4b883e16a93d59a031ce42d7e1
unknown <hasinadilruba2021@gmail.com>
2022-12-16 21:49:52 +0100

    get reviews by id

--- index.js
+++ index.js
@@ -118,6 +118,13 @@ async function run(){
       res.send(result);
    });
 
+   app.get('/reviews/:id', async(req, res) => {
+    const id = req.params.id;
+    const query = {_id: ObjectId(id)};
+    const review = await reviewCollection.findOne(query);
+    res.send(review);
+  });
+
   }
   finally{
   }
